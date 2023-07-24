e21c12f443cf79f2e116ecd60c18a5f36ac8227e
sensiogromaz <crescencio.gromaz@gmail.com>
2023-07-24 23:48:48 +0200

    Prework Python exercises
    Primeros ejercicios

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+"""
+1.1 Crea una variable llamada miEdad y asígna el valor 25.
+1.2 Crea una variable llamada "miNombre" y asigna el valor "Juan" a ella
+1.3 Crea una variable llamada "miPais" y asigna el valor "Argentina" a ella
+1.4 Crea una variable "miPresentacion" y asignale el valor de una cadena que incluya las varibles
+"miNombre" y "miPais".
+"""
+miEdad =25 
+print(miEdad)
+miNombre="Juan"
+print(miNombre)
+miPais = "Argentina"
+print(miPais)
+miPresentación = miNombre + " " + miPais
+print(miPresentación)
